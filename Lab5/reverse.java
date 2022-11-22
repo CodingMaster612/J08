1347f83ea1ed5a5b12e16943ab6197eaf0aa344c
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2022-11-22 07:36:12 -0600

    log

--- Lab5/reverse.java
+++ Lab5/reverse.java
@@ -5,5 +5,27 @@ public class reverse {
         String countMe = "how are you";
         countMe.toCharArray();
 
+        // Append whitespace for the extraction of last word
+        countMe += " ";
+
+        String reversed = "";
+
+        // repeat until no word is left
+        while (countMe.indexOf(" ") != -1) {
+            // Extract word
+            int idx = countMe.indexOf(" ");
+            String word = countMe.substring(0, idx);
+
+            // Concatenate in reverse order
+            reversed = word + " " + reversed;
+
+            // Remove word from the sentence
+            countMe = countMe.substring(idx + 1);
+        }
+
+        // Output the reverse
+        System.out.print("Reverse: ");
+        System.out.println(reversed);
     }
+
 }
