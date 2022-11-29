20ea937ff1732cb2182e72e2482b9f43b46b7027
CodingMaster612 <105937944+CodingMaster612@users.noreply.github.com>
2022-11-29 08:03:35 -0600

    log

--- Practice/Arrays.java
+++ Practice/Arrays.java
@@ -4,4 +4,17 @@ public class Arrays {
     public static void main(String[] args) {
 
     }
+    // public class Dog extends Animal{
+
+    // }
+    // public name ();
+    // geter s and setters : source action
+    // private vaiables
+    // dog peace = new dog("peace")
+    // System.out.println(peace.name)
+
+    // public dog(string name){
+    // super(name);
+    // }
+
 }
